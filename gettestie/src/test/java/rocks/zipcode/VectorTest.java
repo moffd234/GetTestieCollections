28df51ecf5aa4ef7b567cf1462a757ecac81b72f
moffd234 <moffd234@gmail.com>
2024-03-17 15:01:07 -0400

    Completed VectorTest:

--- gettestie/src/test/java/rocks/zipcode/VectorTest.java
+++ gettestie/src/test/java/rocks/zipcode/VectorTest.java
@@ -1,4 +1,66 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Vector;
+
 public class VectorTest {
+
+    @Test
+    public void testTrim(){
+        Vector<Integer> vector = new Vector<Integer>(500);
+        Integer expected = 1;
+
+        vector.add(1);
+        vector.trimToSize();
+        Integer actual = vector.size();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsEmpty(){
+        Vector<Integer> vector = new Vector<Integer>(500);
+
+        Assert.assertTrue(vector.isEmpty());
+    }
+
+    @Test
+    public void testElementAt(){
+        Vector<Integer> vector = new Vector<Integer>(500);
+        int expected = 230;
+
+        vector.add(0, 23);
+        vector.add(1, 24);
+        vector.add(2, 230);
+        vector.add(3, 14);
+
+        int actual = vector.elementAt(2);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testClone(){
+        Vector<Integer> vector = new Vector<Integer>(500);
+        vector.add(0, 23);
+        vector.add(1, 24);
+        vector.add(2, 230);
+        vector.add(3, 14);
+
+        Vector<Integer> newVector = (Vector<Integer>) vector.clone();
+
+        Assert.assertEquals(newVector, vector);
+    }
+
+    @Test
+    public void testCapacity(){
+        Vector<Integer> vector = new Vector<Integer>(500);
+        int expected = 500;
+
+        int actual = vector.capacity();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
