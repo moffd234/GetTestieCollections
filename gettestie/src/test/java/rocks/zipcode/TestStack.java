6874973f1a4c19c4b526e396355a44573a6610e1
moffd234 <moffd234@gmail.com>
2024-03-17 14:49:21 -0400

    Completed StackTest

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -1,13 +1,17 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
+import java.nio.channels.AsynchronousServerSocketChannel;
 import java.util.Stack;
 
 public class TestStack {
 
     @org.junit.Before
     public void setUp() throws Exception {
-
     }
 
     @org.junit.After
@@ -18,8 +22,57 @@ public class TestStack {
     public void TestStack1() {
         Stack<String> stack = new Stack<>();
         stack.push("Hello world");
-        assertEquals(false, stack.isEmpty()); // false
+        assertFalse(stack.isEmpty()); // false
     }
 
-    // Make a bigger test exercising more Stack methods.....
+    @org.junit.Test
+    public void testPop(){
+
+        Stack<Integer> stack = new Stack<>();
+
+        Integer expected = 2;
+
+        stack.add(1);
+        stack.add(3);
+        stack.add(2);
+        Integer actual = stack.pop();
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testPush(){
+        Stack<Integer> stack = new Stack<>();
+
+        Integer expected = 2;
+
+        stack.push(1);
+        stack.push(3);
+        stack.push(2);
+        Integer actual = stack.pop();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testPeek(){
+        Stack<Integer> stack = new Stack<>();
+
+        Integer expected = 2;
+
+        stack.add(1);
+        stack.add(3);
+        stack.add(2);
+        Integer actual = stack.peek();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsEmpty(){
+        Stack<Integer> stack = new Stack<>();
+        Assert.assertTrue(stack.isEmpty());
+    }
+
 }
